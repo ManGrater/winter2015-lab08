0974ce3a1d83c26a5431c968deebf7725182ab27
Paolo Montano <Paolo.Montano@Gmail.com>
2015-04-08 11:05:30 -0700

    Auth controllers.

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -14,7 +14,7 @@ class Auth extends Application {
   function submit() {
     $key = $_POST['userid'];
     $user = $this->Users->get($key);
-    if (password_verify($this->input->post('password'), $user->password)) {
+   if (password_verify($_POST['password'], $user->password)) {
       $this->session->set_userdata('userID', $key);
       $this->session->set_userdata('userName', $user->name);
       $this->session->set_userdata('userRole', $user->role);
